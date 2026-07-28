2bae47f008d51ede7eec408435362958dd149332
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-07-28 12:42:43 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,5 @@
+class LandingPageCopyClient:
+    def optimize_copy(self, headline: str) -> dict:
+        return {
+            "suggested_headline": 'Supercharge Your Workflow in 30 Seconds'
+        }
